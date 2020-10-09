5c0645cfd702896dd920df067f193d8b88a5f6a5
rainot <loic.rainot@ag2rlamondiale.fr>
2020-10-09 15:28:55 +0200

    Beginning with azerty

---
+++ examples/Devices/Keyboardio/AtreusBepo/AtreusBepo.ino
@@ -0,0 +1,422 @@
+/* -*- mode: c++ -*-
+ * Atreus -- Chrysalis-enabled Sketch for the Keyboardio Atreus
+ * Copyright (C) 2018, 2019  Keyboard.io, Inc
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef BUILD_INFORMATION
+#define BUILD_INFORMATION "Bepo V1"
+#endif
+
+#include "Kaleidoscope.h"
+#include "Kaleidoscope-EEPROM-Settings.h"
+#include "Kaleidoscope-EEPROM-Keymap.h"
+#include "Kaleidoscope-FocusSerial.h"
+#include "Kaleidoscope-Macros.h"
+#include "Kaleidoscope-MouseKeys.h"
+#include "Kaleidoscope-OneShot.h"
+#include "Kaleidoscope-Qukeys.h"
+#include "Kaleidoscope-SpaceCadet.h"
+#include "Kaleidoscope-HostOS.h"
+#include "Kaleidoscope-Unicode.h"
+#include "Kaleidoscope-MagicCombo.h"
+
+
+
+
+#define MO(n) ShiftToLayer(n)
+#define TG(n) LockLayer(n)
+
+enum {
+  CHANGE_OS
+};
+
+enum {
+  MACRO_QWERTY,
+  MACRO_VERSION_INFO,
+  KEY_EACUTE,
+  KEY_EGRAVE,
+  KEY_COMMA,
+  KEY_AGRAVE,
+  KEY_DOT,
+  KEY_CCEDILLA,
+  KEY_CIRCUMFLEX,
+  KEY_AT,
+  KEY_PIPE,
+  KEY_AMPERSAND,
+  KEY_OELIG,
+  KEY_AELIG,
+  KEY_UGRAVE,
+  KEY_UMLAUT,
+  KEY_EURO,
+  KEY_BACKSLASH,
+  KEY_LBRACES,
+  KEY_RBRACES,
+  KEY_TRIPLEDOT,
+  KEY_TILDE,
+  KEY_OPENQUOTE,
+  KEY_CLOSEQUOTE,
+  KEY_APOSTROPHE
+};
+
+#define Key_Exclamation LSHIFT(Key_1)
+#define Key_At LSHIFT(Key_2)
+#define Key_Hash LSHIFT(Key_3)
+#define Key_Dollar LSHIFT(Key_4)
+#define Key_Percent LSHIFT(Key_5)
+#define Key_Caret LSHIFT(Key_6)
+#define Key_And LSHIFT(Key_7)
+#define Key_Star LSHIFT(Key_8)
+#define Key_Plus LSHIFT(Key_Equals)
+
+enum {
+  QWERTY,
+  FUN,
+  UPPER,
+  BEPO,
+  ALT_GR,
+  LEFT_LAYER,
+  RIGHT_LAYER
+};
+
+/* *INDENT-OFF* */
+KEYMAPS(
+  [QWERTY] = KEYMAP_STACKED
+  (
+       Key_Q   ,Key_W   ,Key_E       ,Key_R         ,Key_T
+      ,Key_A   ,Key_S   ,Key_D       ,Key_F         ,Key_G
+      ,Key_Z   ,Key_X   ,Key_C       ,Key_V         ,Key_B, Key_Backtick
+      ,Key_Esc ,Key_Tab ,Key_LeftGui ,Key_LeftShift ,Key_Backspace ,Key_LeftControl
+
+                     ,Key_Y     ,Key_U      ,Key_I     ,Key_O      ,Key_P
+                     ,Key_H     ,Key_J      ,Key_K     ,Key_L      ,Key_Semicolon
+       ,Key_Backslash,Key_N     ,Key_M      ,Key_Comma ,Key_Period ,Key_Slash
+       ,Key_LeftAlt  ,Key_Space ,MO(FUN)    ,Key_Minus ,Key_Quote  ,Key_Enter
+  ),
+
+  [FUN] = KEYMAP_STACKED
+  (
+       Key_Exclamation ,Key_At           ,Key_UpArrow   ,Key_Dollar           ,Key_Percent
+      ,Key_LeftParen   ,Key_LeftArrow    ,Key_DownArrow ,Key_RightArrow       ,Key_RightParen
+      ,Key_LeftBracket ,Key_RightBracket ,Key_Hash      ,Key_LeftCurlyBracket ,Key_RightCurlyBracket ,Key_Caret
+      ,TG(UPPER)       ,Key_Insert       ,Key_LeftGui   ,Key_LeftShift        ,Key_Delete         ,Key_LeftControl
+
+                   ,Key_PageUp   ,Key_7 ,Key_8      ,Key_9 ,Key_Backspace
+                   ,Key_PageDown ,Key_4 ,Key_5      ,Key_6 ,___
+      ,Key_And     ,Key_Star     ,Key_1 ,Key_2      ,Key_3 ,Key_Plus
+      ,Key_LeftAlt ,Key_Space    ,___   ,Key_Period ,Key_0 ,Key_Equals
+   ),
+
+  [UPPER] = KEYMAP_STACKED
+  (
+       Key_Insert            ,Key_Home                 ,Key_UpArrow   ,Key_End        ,Key_PageUp
+      ,Key_Delete            ,Key_LeftArrow            ,Key_DownArrow ,Key_RightArrow ,Key_PageDown
+      ,M(MACRO_VERSION_INFO) ,Consumer_VolumeIncrement ,XXX           ,XXX            ,___ ,___
+      ,MoveToLayer(QWERTY)   ,Consumer_VolumeDecrement ,___           ,___            ,___ ,___
+
+                ,Key_UpArrow   ,Key_F7              ,Key_F8          ,Key_F9         ,Key_F10
+                ,Key_DownArrow ,Key_F4              ,Key_F5          ,Key_F6         ,Key_F11
+      ,___      ,XXX           ,Key_F1              ,Key_F2          ,Key_F3         ,Key_F12
+      ,MoveToLayer(BEPO)      ,___           ,MoveToLayer(QWERTY) ,Key_PrintScreen ,Key_ScrollLock ,Consumer_PlaySlashPause
+   ),
+
+   [BEPO] = KEYMAP_STACKED
+  (
+       Key_B          ,M(KEY_EACUTE)  ,Key_P       ,Key_O         ,M(KEY_EGRAVE)
+      ,Key_Q          ,Key_U          ,Key_I       ,Key_E         ,M(KEY_COMMA)
+      ,M(KEY_AGRAVE)  ,Key_Y          ,Key_X       ,M(KEY_DOT)    ,Key_K         ,M(KEY_CCEDILLA)
+      ,Key_Esc        ,Key_Tab        ,Key_LeftGui ,Key_LeftShift ,Key_Backspace ,Key_Z
+
+                      ,M(KEY_CIRCUMFLEX)  ,Key_V          ,Key_D            ,Key_L         ,Key_J
+                      ,Key_C              ,Key_T          ,Key_S            ,Key_R         ,Key_N
+       ,Key_W         ,M(KEY_APOSTROPHE)  ,Key_A          ,Key_G            ,Key_H         ,Key_F
+       ,Key_Semicolon ,Key_Space          ,Key_6   ,MoveToLayer(QWERTY)     ,M(KEY_AT)     ,Key_Enter
+   ),
+
+   [ALT_GR] = KEYMAP_STACKED
+  (
+       M(KEY_PIPE)      ,___            ,M(KEY_AMPERSAND) ,M(KEY_OELIG)      ,___
+      ,M(KEY_AELIG)     ,M(KEY_UGRAVE)  ,M(KEY_UMLAUT)    ,M(KEY_EURO)       ,Key_4
+      ,M(KEY_BACKSLASH) ,M(KEY_LBRACES) ,M(KEY_RBRACES)   ,M(KEY_TRIPLEDOT)  ,M(KEY_TILDE)      ,___
+      ,___              ,___            ,___              ,___               ,Key_8             ,___
+
+               ,___    ,___                ,___     ,___      ,___
+               ,___    ,Key_LeftShift      ,___     ,___      ,___
+       ,___    ,___    ,___                ,___     ,___      ,___
+       ,___    ,___    ,___                ,___     ,___      ,___
+   ),
+
+  [LEFT_LAYER] = KEYMAP_STACKED
+  (
+       Key_Keypad1         ,Key_Keypad2            ,Key_Keypad3        ,Key_Keypad4     ,Key_Keypad5
+      ,Key_Backslash       ,RALT(Key_3)            ,Key_5              ,Key_Minus       ,___
+      ,LSHIFT(Key_Period)  ,Key_RightBracket  ,RALT(Key_5)        ,RALT(Key_Minus) ,___            ,___
+      ,___                 ,___                    ,___                ,___             ,Key_Backspace  ,Key_Delete
+
+               ,___    ,___            ,Key_UpArrow     ,___             ,___
+               ,___    ,Key_LeftArrow  ,Key_DownArrow   ,Key_RightArrow  ,Key_LeftShift
+       ,___    ,___    ,___            ,___             ,___             ,___
+       ,___    ,___    ,___            ,___             ,___             ,___
+   ),
+
+   [RIGHT_LAYER] = KEYMAP_STACKED
+  (
+       ___   ,___   ,___   ,___            ,___
+      ,___   ,___   ,___   ,Key_LeftShift  ,___
+      ,___   ,___   ,___   ,___            ,___    ,___
+      ,___   ,___   ,___   ,___            ,___    ,___
+
+             ,Key_Keypad6  ,Key_Keypad7                 ,Key_Keypad8                          ,Key_Keypad9                    ,Key_Keypad0
+             ,___          ,Key_NonUsBackslashAndPipe   ,LSHIFT(Key_NonUsBackslashAndPipe)    ,Key_3                          ,Key_Equals
+      ,___   ,___          ,M(KEY_OPENQUOTE)            ,M(KEY_CLOSEQUOTE)                    ,LSHIFT(Key_RightBracket)  ,LSHIFT(Key_Quote)
+      ,___   ,___          ,___                         ,___                                  ,___                            ,___
+   )  
+)
+/* *INDENT-ON* */
+
+void change_os(uint8_t combo_index) {
+    if (HostOS.os() == kaleidoscope::hostos::LINUX) {
+      HostOS.os(kaleidoscope::hostos::WINDOWS);
+    }
+    else {
+      HostOS.os(kaleidoscope::hostos::LINUX);
+    }
+}
+
+USE_MAGIC_COMBOS([CHANGE_OS] = {.action = change_os, .keys = {R3C0, R3C3}});
+
+KALEIDOSCOPE_INIT_PLUGINS(
+  HostOS,
+  EEPROMSettings,
+  EEPROMKeymap,
+  Qukeys,
+  Focus,
+  FocusEEPROMCommand,
+  FocusSettingsCommand,
+  OneShot,
+  SpaceCadet,
+  MouseKeys,
+  MagicCombo,
+  Macros
+);
+
+const macro_t *macroAction(uint8_t macroIndex, uint8_t keyState) {
+  switch (macroIndex) {
+  case MACRO_QWERTY:
+    // This macro is currently unused, but is kept around for compatibility
+    // reasons. We used to use it in place of `MoveToLayer(QWERTY)`, but no
+    // longer do. We keep it so that if someone still has the old layout with
+    // the macro in EEPROM, it will keep working after a firmware update.
+    Layer.move(QWERTY);
+    break;
+  case MACRO_VERSION_INFO:
+    if (keyToggledOn(keyState)) {
+      Macros.type(PSTR("Keyboardio Atreus - Kaleidoscope "));
+      Macros.type(PSTR(BUILD_INFORMATION));
+    }
+    break;
+  case KEY_EGRAVE:
+     if (HostOS.os() == kaleidoscope::hostos::LINUX)
+       if (kaleidoscope::hid::wasModifierKeyActive(Key_LeftShift))
+        return MACRODOWN(I(5), D(RightAlt), T(7), U(RightAlt));
+      else
+        return MACRODOWN(I(5), T(7));
+    else {
+      if (kaleidoscope::hid::wasModifierKeyActive(Key_LeftShift))
+        return MACRODOWN(I(5), U(LeftShift), D(LeftAlt), T(Keypad0), T(Keypad2), T(Keypad0), T(Keypad0), U(LeftAlt), D(LeftShift));
+      else
+        return MACRODOWN(I(5), T(7));
+    }
+    break;
+  case KEY_EACUTE:
+     if (HostOS.os() == kaleidoscope::hostos::LINUX)
+       if (kaleidoscope::hid::wasModifierKeyActive(Key_LeftShift))
+        return MACRODOWN(I(5), D(RightAlt), T(2), U(RightAlt));
+      else
+        return MACRODOWN(I(5), T(2));
+    else {
+      if (kaleidoscope::hid::wasModifierKeyActive(Key_LeftShift))
+        return MACRODOWN(I(5), U(LeftShift), D(LeftAlt), T(Keypad1), T(Keypad4), T(Keypad4), U(LeftAlt), D(LeftShift));
+      else
+        return MACRODOWN(I(5), T(2));
+    }
+    break;
+  case KEY_COMMA:
+    if (kaleidoscope::hid::wasModifierKeyActive(Key_LeftShift))
+      return MACRODOWN(I(5), U(LeftShift), T(Comma), D(LeftShift));
+    else
+      return MACRODOWN(I(5), T(M));
+    break;
+  case KEY_AGRAVE:
+     if (HostOS.os() == kaleidoscope::hostos::LINUX)
+       if (kaleidoscope::hid::wasModifierKeyActive(Key_LeftShift))
+        return MACRODOWN(I(5), D(RightAlt), T(0), U(RightAlt));
+      else
+        return MACRODOWN(I(5), T(0));
+    else {
+      if (kaleidoscope::hid::wasModifierKeyActive(Key_LeftShift))
+        return MACRODOWN(I(5), U(LeftShift), D(LeftAlt), T(Keypad0), T(Keypad1), T(Keypad9), T(Keypad2), U(LeftAlt), D(LeftShift));
+      else
+        return MACRODOWN(I(5), T(0));
+    }
+    break;
+  case KEY_DOT:
+    if (kaleidoscope::hid::wasModifierKeyActive(Key_LeftShift))
+      return MACRODOWN(I(5), U(LeftShift), T(Period), U(LeftShift));
+    else
+      return MACRODOWN(I(5), D(LeftShift), T(Comma), U(LeftShift));
+    break;
+  case KEY_CCEDILLA:
+     if (HostOS.os() == kaleidoscope::hostos::LINUX)
+       if (kaleidoscope::hid::wasModifierKeyActive(Key_LeftShift))
+        return MACRODOWN(I(5), D(RightAlt), T(9), U(RightAlt));
+      else
+        return MACRODOWN(I(5), T(9));
+    else {
+      if (kaleidoscope::hid::wasModifierKeyActive(Key_LeftShift))
+        return MACRODOWN(I(5), U(LeftShift), D(LeftAlt), T(Keypad0), T(Keypad1), T(Keypad9), T(Keypad9), U(LeftAlt), D(LeftShift));
+      else
+        return MACRODOWN(I(5), T(9));
+    }
+    break;
+  case KEY_CIRCUMFLEX:
+    if (kaleidoscope::hid::wasModifierKeyActive(Key_LeftShift))
+     return MACRODOWN(U(LeftShift), T(Slash), D(LeftShift));
+    else
+     return MACRODOWN(T(LeftBracket));
+    break;
+  case KEY_AT:
+    return MACRODOWN(I(5), D(RightAlt), T(0), U(RightAlt));
+    break;
+  case KEY_PIPE:
+    return MACRODOWN(I(5), D(RightAlt), T(6), U(RightAlt));
+    break;
+  case KEY_AMPERSAND:
+    return MACRODOWN(I(5), T(1));
+    break;
+  case KEY_OELIG:
+    if (HostOS.os() == kaleidoscope::hostos::LINUX)  
+      return MACRODOWN(I(5), D(RightAlt), T(O), U(RightAlt));
+    else {
+      if (kaleidoscope::hid::wasModifierKeyActive(Key_LeftShift))
+        return MACRODOWN(I(5), U(LeftShift), D(LeftAlt), T(Keypad0), T(Keypad1), T(Keypad4), T(Keypad0), U(LeftAlt), D(LeftShift));
+      else
+        return MACRODOWN(I(5), D(LeftAlt), T(Keypad0), T(Keypad1), T(Keypad5), T(Keypad6), U(LeftAlt));
+    }
+    break;
+  case KEY_AELIG:
+    if (HostOS.os() == kaleidoscope::hostos::LINUX) 
+      return MACRODOWN(I(5), D(RightAlt), T(Q), U(RightAlt));
+    else {
+      if (kaleidoscope::hid::wasModifierKeyActive(Key_LeftShift))
+        return MACRODOWN(I(5), U(LeftShift), D(LeftAlt), T(Keypad1), T(Keypad4), T(Keypad6), U(LeftAlt), D(LeftShift));
+      else
+        return MACRODOWN(I(5), D(LeftAlt), T(Keypad1), T(Keypad4), T(Keypad5), U(LeftAlt));
+    }
+    break;
+  case KEY_UGRAVE:
+      if (HostOS.os() == kaleidoscope::hostos::LINUX)
+       if (kaleidoscope::hid::wasModifierKeyActive(Key_LeftShift))
+        return MACRODOWN(I(5), D(RightAlt), T(Quote), U(RightAlt));
+      else
+        return MACRODOWN(I(5), T(Quote));
+    else {
+      if (kaleidoscope::hid::wasModifierKeyActive(Key_LeftShift))
+        return MACRODOWN(I(5), U(LeftShift), D(LeftAlt), T(Keypad0), T(Keypad2), T(Keypad1), T(Keypad7), U(LeftAlt), D(LeftShift));
+      else
+        return MACRODOWN(I(5), T(Quote));
+    }
+    break;
+  case KEY_UMLAUT:
+    return MACRODOWN(I(5), D(LeftShift), T(LeftCurlyBracket), U(LeftShift));
+    break;
+  case KEY_EURO:
+    return MACRODOWN(I(5), D(RightAlt), T(E), U(RightAlt));
+    break;
+  case KEY_BACKSLASH:
+    return MACRODOWN(I(5), D(RightAlt), T(8), U(RightAlt));
+    break;
+  case KEY_LBRACES:
+    return MACRODOWN(I(5), D(RightAlt), T(4), U(RightAlt));
+    break;
+  case KEY_RBRACES:
+    return MACRODOWN(I(5), D(RightAlt), T(Equals), U(RightAlt));
+    break;
+  case KEY_TRIPLEDOT:
+      if (HostOS.os() == kaleidoscope::hostos::LINUX)
+        return MACRODOWN(I(5), D(LeftShift), D(RightAlt), T(M), U(RightAlt), U(LeftShift));
+    else 
+        return MACRODOWN(I(5), D(LeftAlt), T(Keypad0), T(Keypad1), T(Keypad3), T(Keypad3), U(LeftAlt));
+    break;
+  case KEY_TILDE:
+    return MACRODOWN(I(5), D(RightAlt), T(2), U(RightAlt));
+    break;
+  case KEY_APOSTROPHE:
+     if (HostOS.os() == kaleidoscope::hostos::LINUX)
+       if (kaleidoscope::hid::wasModifierKeyActive(Key_LeftShift))
+        return MACRODOWN(I(5), T(M));
+      else
+        return MACRODOWN(I(5), D(RightAlt), T(G), U(RightAlt));
+    else {
+      if (kaleidoscope::hid::wasModifierKeyActive(Key_LeftShift))
+        return MACRODOWN(I(5), T(M));
+      else
+        return MACRODOWN(I(5), D(LeftAlt), T(Keypad0), T(Keypad1), T(Keypad4), T(Keypad6), U(LeftAlt));
+    }
+    break;
+  case KEY_OPENQUOTE:
+    if (HostOS.os() == kaleidoscope::hostos::LINUX)
+        return MACRODOWN(I(5), D(RightAlt), T(Z), U(RightAlt));
+    else 
+        return MACRODOWN(I(5), D(LeftAlt), T(Keypad1), T(Keypad7), T(Keypad4), U(LeftAlt));
+    break;
+  case KEY_CLOSEQUOTE:
+    if (HostOS.os() == kaleidoscope::hostos::LINUX)
+        return MACRODOWN(I(5), D(RightAlt), T(X), U(RightAlt));
+    else 
+        return MACRODOWN(I(5), D(LeftAlt),  T(Keypad1), T(Keypad7), T(Keypad5), U(LeftAlt));
+    break;
+  default:
+    break;
+  }
+  return MACRO_NONE;
+}
+
+void setup() {
+  EEPROMKeymap.setup(10);
+
+  QUKEYS(
+    kaleidoscope::plugin::Qukey(BEPO, KeyAddr(1, 3), Key_LeftShift),             // E/shift
+    kaleidoscope::plugin::Qukey(BEPO, KeyAddr(1, 8), Key_LeftShift),             // T/shift
+    kaleidoscope::plugin::Qukey(BEPO, KeyAddr(1, 11), Key_LeftControl),          // N/ctrl
+    kaleidoscope::plugin::Qukey(BEPO, KeyAddr(3, 6), ShiftToLayer(ALT_GR)),      // M/layer-shift (on `alt_gr`)
+    kaleidoscope::plugin::Qukey(BEPO, KeyAddr(3, 7), ShiftToLayer(LEFT_LAYER)),  // Space/layer-shift (on `left_layer`)
+    kaleidoscope::plugin::Qukey(BEPO, KeyAddr(3, 4), ShiftToLayer(RIGHT_LAYER))  // Backspace/layer-shift (on `right_layer`)
+  )
+
+  Qukeys.setHoldTimeout(1000);
+  Qukeys.setOverlapThreshold(80);
+  Qukeys.activate();
+
+  HostOS.os(kaleidoscope::hostos::WINDOWS);
+  
+  Kaleidoscope.setup();
+  SpaceCadet.disable();
+}
+
+void loop() {
+  Kaleidoscope.loop();
+}
---
+++ src/kaleidoscope/lang/fr_azerty.h
@@ -0,0 +1,287 @@
+#pragma once
+
+// Alt gr
+#define ALTGR RALT
+
+/* These are deviating from the us layout */
+#define Fr_Circumflex Key_LeftBracket /* ^ and ° */
+#define Fr_Trema LSHIFT(Fr_Circumflex) /* ° */
+
+#define Fr_ExlamationMark Key_1
+#define Fr_DoubleQuote LSHIFT(Fr_2)
+#define Fr_Paragraph LSHIFT(Fr_3)
+#define Fr_Dollar LSHIFT(Fr_4)
+#define Fr_Percent LSHIFT(Fr_5)
+#define Fr_Ampersand LSHIFT(Fr_6)
+#define Fr_Slash LSHIFT(Fr_7)
+#define Fr_LeftParen LSHIFT(Fr_8)
+#define Fr_RightParen LSHIFT(Fr_9)
+#define Fr_Equals LSHIFT(Fr_0)
+#define Fr_QuestionMark LSHIFT(Fr_SZ)
+#define Fr_LeftCurly ALTGR(Fr_7)
+#define Fr_RightCurly ALTGR(Fr_0)
+#define Fr_LeftBracket ALTGR(Fr_8)
+#define Fr_RightBracket ALTGR(Fr_9)
+#define Fr_Backslash ALTGR(Fr_SZ)
+
+#define Fr_Tick Key_Equals /* Tick ´ and Backtick ` */
+#define Fr_Backtick LSHIFT(Fr_Tick)
+
+#define Fr_At ALTGR(Fr_Q)
+#define Fr_Euro ALTGR(Fr_E)
+
+#define Fr_Plus Key_RightBracket /* Plus + Asterisk * and Tilde ~ */
+#define Fr_Asterisk LSHIFT(Fr_Plus) /* * */
+#define Fr_Tilde ALTGR(Fr_Plus) /* ~ */
+
+#define Fr_Hash ALTGR(Key_3) /* # */
+#define Fr_Quote Key_4
+
+#define Fr_LessThan Key_NonUsBackslashAndPipe /* < and > and | */
+#define Fr_GreaterThan LSHIFT(Fr_LessThan)
+#define Fr_Pipe ALTGR(Key_6)
+
+#define Fr_Minus Key_Slash /* - and _ */
+#define Fr_Underscore LSHIFT(Fr_Minus)
+
+/* the default keys are just aliased to have them look the same */
+#define Fr_NoEvent Key_NoEvent
+#define Fr_ErrorRollover Key_ErrorRollover
+#define Fr_PostFail Key_PostFail
+#define Fr_ErrorUndefined Key_ErrorUndefined
+#define Fr_A Key_Q
+#define Fr_B Key_B
+#define Fr_C Key_C
+#define Fr_D Key_D
+#define Fr_E Key_E
+#define Fr_F Key_F
+#define Fr_G Key_G
+#define Fr_H Key_H
+#define Fr_I Key_I
+#define Fr_J Key_J
+#define Fr_K Key_K
+#define Fr_L Key_L
+#define Fr_M Key_Comma
+#define Fr_N Key_N
+#define Fr_O Key_O
+#define Fr_P Key_P
+#define Fr_Q Key_A
+#define Fr_R Key_R
+#define Fr_S Key_S
+#define Fr_T Key_T
+#define Fr_U Key_U
+#define Fr_V Key_V
+#define Fr_W Key_Z
+#define Fr_X Key_X
+#define Fr_Y Key_Y
+#define Fr_Z Key_W
+
+#define Fr_1 LSHIFT(Key_1)
+#define Fr_2 LSHIFT(Key_2)
+#define Fr_3 LSHIFT(Key_3)
+#define Fr_4 LSHIFT(Key_4)
+#define Fr_5 LSHIFT(Key_5)
+#define Fr_6 LSHIFT(Key_6)
+#define Fr_7 LSHIFT(Key_7)
+#define Fr_8 LSHIFT(Key_8)
+#define Fr_9 LSHIFT(Key_9)
+#define Fr_0 LSHIFT(Key_0)
+#define Fr_Enter Key_Enter
+#define Fr_Escape Key_Escape
+#define Fr_Backspace Key_Backspace
+#define Fr_Tab Key_Tab
+#define Fr_Spacebar Key_Spacebar
+#define Fr_Comma Key_M
+#define Fr_Semicolon LSHIFT(Fr_Comma)
+#define Fr_Period Key_Period
+#define Fr_Colon LSHIFT(Fr_Period)
+
+#define Fr_CapsLock Key_CapsLock
+#define Fr_F1 Key_F1
+#define Fr_F2 Key_F2
+#define Fr_F3 Key_F3
+#define Fr_F4 Key_F4
+#define Fr_F5 Key_F5
+#define Fr_F6 Key_F6
+#define Fr_F7 Key_F7
+#define Fr_F8 Key_F8
+#define Fr_F9 Key_F9
+#define Fr_F10 Key_F10
+#define Fr_F11 Key_F11
+#define Fr_F12 Key_F12
+
+#define Fr_PrintScreen Key_PrintScreen
+#define Fr_Drucken Fr_PrintScreen
+#define Fr_Druck Fr_PrintScreen
+#define Fr_ScrollLock Key_ScrollLock
+#define Fr_Rollen Fr_ScrollLock
+#define Fr_Pause Key_Pause
+#define Fr_Insert Key_Insert
+#define Fr_Einfuegen Fr_Insert
+#define Fr_Einfg Fr_Insert
+#define Fr_Home Key_Home
+#define Fr_Position1 Fr_Home
+#define Fr_Pos1 Fr_Home
+#define Fr_PageUp Key_PageUp
+#define Fr_BildAuf Fr_PageUp
+#define Fr_Delete Key_Delete
+#define Fr_Entfernen Fr_Delete
+#define Fr_Entf Fr_Delete
+#define Fr_End Key_End
+#define Fr_Ende Fr_End
+#define Fr_PageDown Key_PageDown
+#define Fr_BildAb Fr_PageDown
+#define Fr_RightArrow Key_RightArrow
+#define Fr_LeftArrow Key_LeftArrow
+#define Fr_DownArrow Key_DownArrow
+#define Fr_UpArrow Key_UpArrow
+
+#define Fr_KeypadNumLock Key_KeypadNumLock
+#define Fr_KeypadDivide Key_KeypadDivide
+#define Fr_KeypadMultiply Key_KeypadMultiply
+#define Fr_KeypadSubtract Key_KeypadSubtract
+#define Fr_KeypadAdd Key_KeypadAdd
+#define Fr_KeypadEnter Key_KeypadEnter
+#define Fr_Keypad1 Key_Keypad1
+#define Fr_Keypad2 Key_Keypad2
+#define Fr_Keypad3 Key_Keypad3
+#define Fr_Keypad4 Key_Keypad4
+#define Fr_Keypad5 Key_Keypad5
+#define Fr_Keypad6 Key_Keypad6
+#define Fr_Keypad7 Key_Keypad7
+#define Fr_Keypad8 Key_Keypad8
+#define Fr_Keypad9 Key_Keypad9
+#define Fr_Keypad0 Key_Keypad0
+
+/* German uses comma for decimals */
+#define Fr_KeypadComma Key_KeypadDot
+
+#define Fr_PcApplication Key_PcApplication
+#define Fr_Power Key_Power
+
+#define Fr_KeypadEquals Key_KeypadEquals
+
+#define Fr_F13 Key_F13
+#define Fr_F14 Key_F14
+#define Fr_F15 Key_F15
+#define Fr_F16 Key_F16
+#define Fr_F17 Key_F17
+#define Fr_F18 Key_F18
+#define Fr_F19 Key_F19
+#define Fr_F20 Key_F20
+#define Fr_F21 Key_F21
+#define Fr_F22 Key_F22
+#define Fr_F23 Key_F23
+#define Fr_F24 Key_F24
+
+#define Fr_Execute Key_Execute
+#define Fr_Help Key_Help
+#define Fr_Menu Key_Menu
+#define Fr_Select Key_Select
+#define Fr_Stop Key_Stop
+#define Fr_Again Key_Again
+#define Fr_Undo Key_Undo
+#define Fr_Cut Key_Cut
+#define Fr_Copy Key_Copy
+#define Fr_Paste Key_Paste
+#define Fr_Find Key_Find
+
+#define Fr_Mute Key_Mute
+#define Fr_VolumeUp Key_VolumeUp
+#define Fr_VolumeDown Key_VolumeDown
+
+#define Fr_LockingCapsLock Key_LockingCapsLock
+#define Fr_LockingNumLock Key_LockingNumLock
+#define Fr_LockingScrollLock Key_LockingScrollLock
+
+#define Fr_KeypadDot Key_KeypadComma
+#define Fr_KeypadEqualSign Key_KeypadEqualSign
+
+#define Fr_International1 Key_International1
+#define Fr_International2 Key_International2
+#define Fr_International3 Key_International3
+#define Fr_International4 Key_International4
+#define Fr_International5 Key_International5
+#define Fr_International6 Key_International6
+#define Fr_International7 Key_International7
+#define Fr_International8 Key_International8
+#define Fr_International9 Key_International9
+#define Fr_Lang1 Key_Lang1
+#define Fr_Lang2 Key_Lang2
+#define Fr_Lang3 Key_Lang3
+#define Fr_Lang4 Key_Lang4
+#define Fr_Lang5 Key_Lang5
+#define Fr_Lang6 Key_Lang6
+#define Fr_Lang7 Key_Lang7
+#define Fr_Lang8 Key_Lang8
+#define Fr_Lang9 Key_Lang9
+
+#define Fr_AlternateErase Key_AlternateErase
+#define Fr_Sysreq Key_Sysreq
+#define Fr_Cancel Key_Cancel
+#define Fr_Clear Key_Clear
+#define Fr_Prior Key_Prior
+#define Fr_Return Key_Return
+#define Fr_Separator Key_Separator
+#define Fr_Out Key_Out
+#define Fr_Oper Key_Oper
+#define Fr_ClearSlashAgain Key_ClearSlashAgain
+#define Fr_CrselSlashProps Key_CrselSlashProps
+#define Fr_Exsel Key_Exsel
+
+// Reserved	0xA5-AF
+#define Fr_Keypad00 Key_Keypad00
+#define Fr_Keypad000 Key_Keypad000
+#define Fr_ThousandsSeparator Key_ThousandsSeparator
+#define Fr_DecimalSeparator Key_DecimalSeparator
+#define Fr_CurrencyUnit Key_CurrencyUnit
+#define Fr_CurrencySubunit Key_CurrencySubunit
+#define Fr_KeypadLeftParen Key_KeypadLeftParen
+#define Fr_KeypadRightParen Key_KeypadRightParen
+#define Fr_KeypadLeftCurlyBrace Key_KeypadLeftCurlyBrace
+#define Fr_KeypadRightCurlyBrace Key_KeypadRightCurlyBrace
+#define Fr_KeypadTab Key_KeypadTab
+#define Fr_KeypadBackspace Key_KeypadBackspace
+#define Fr_KeypadA Key_KeypadA
+#define Fr_KeypadB Key_KeypadB
+#define Fr_KeypadC Key_KeypadC
+#define Fr_KeypadD Key_KeypadD
+#define Fr_KeypadE Key_KeypadE
+#define Fr_KeypadF Key_KeypadF
+#define Fr_KeypadXor Key_KeypadXor
+#define Fr_KeypadCarat Key_KeypadCarat
+#define Fr_KeypadPercent Key_KeypadPercent
+#define Fr_KeypadLessThan Key_KeypadLessThan
+#define Fr_KeypadGreaterThan Key_KeypadGreaterThan
+#define Fr_KeypadAmpersand Key_KeypadAmpersand
+#define Fr_KeypadDoubleampersand Key_KeypadDoubleampersand
+#define Fr_KeypadPipe Key_KeypadPipe
+#define Fr_KeypadDoublepipe Key_KeypadDoublepipe
+#define Fr_KeypadColon Key_KeypadColon
+#define Fr_KeypadPoundSign Key_KeypadPoundSign
+#define Fr_KeypadSpace Key_KeypadSpace
+#define Fr_KeypadAtSign Key_KeypadAtSign
+#define Fr_KeypadExclamationPoint Key_KeypadExclamationPoint
+#define Fr_KeypadMemoryStore Key_KeypadMemoryStore
+#define Fr_KeypadMemoryRecall Key_KeypadMemoryRecall
+#define Fr_KeypadMemoryClear Key_KeypadMemoryClear
+#define Fr_KeypadMemoryAdd Key_KeypadMemoryAdd
+#define Fr_KeypadMemorySubtract Key_KeypadMemorySubtract
+#define Fr_KeypadMemoryMultiply Key_KeypadMemoryMultiply
+#define Fr_KeypadMemoryDivide Key_KeypadMemoryDivide
+#define Fr_KeypadPlusSlashMinus Key_KeypadPlusSlashMinus
+#define Fr_KeypadClear Key_KeypadClear
+#define Fr_KeypadClearEntry Key_KeypadClearEntry
+#define Fr_KeypadBinary Key_KeypadBinary
+#define Fr_KeypadOctal Key_KeypadOctal
+#define Fr_KeypadDecimal Key_KeypadDecimal
+#define Fr_KeypadHexadecimal Key_KeypadHexadecimal
+
+#define Fr_LeftControl Key_LeftControl
+#define Fr_LeftShift Key_LeftShift
+#define Fr_LeftAlt Key_LeftAlt
+#define Fr_LeftGui Key_LeftGui
+#define Fr_RightControl Key_RightControl
+#define Fr_RightShift Key_RightShift
+#define Fr_RightAlt Key_RightAlt
+#define Fr_RightGui Key_RightGui
